813f4a242c05fe1801a77971ad9cd87156850649
Zafiza1 <zafiza96@gmail.com>
2026-08-22 11:54:11 +0700

    Fix dashboard data display and prepare for Vercel deployment - Add uploadedBy relationship to SuratFile model - Rebuild frontend assets with latest changes - Update Vercel build command

---
+++ public/build/assets/user--4y5B8Om.js
@@ -0,0 +1,11 @@
+import{d as e}from"./app-BSRHmEXo.js";/**
+ * @license lucide-react v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],r=e("loader-circle",c);/**
+ * @license lucide-react v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],d=e("user",o);export{r as L,d as U};

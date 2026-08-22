44b244d20a5774aae41ba11ce11ea144fe4f51e1
Zafiza1 <zafiza96@gmail.com>
2026-08-22 13:23:36 +0700

    Perubahan Error

--- api/index.php
+++ api/index.php
@@ -4,14 +4,4 @@
  * Here is the serverless function entry
  * for deployment with Vercel.
  */
-
-// Disable deprecation warnings for cleaner production output
-error_reporting(E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED);
-
-// Ensure SQLite database exists for Vercel
-$dbPath = '/tmp/database.sqlite';
-if (!file_exists($dbPath)) {
-    touch($dbPath);
-}
-
 require __DIR__.'/../public/index.php';
